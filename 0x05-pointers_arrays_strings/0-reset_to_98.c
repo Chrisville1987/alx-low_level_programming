1cead1d99fb650f2ad24410349749f8ff6090487
chrisville1987 <christopheranumadu@gmail.com>
2022-10-25 13:42:12 -0700

    he low

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,9 @@
   /**
    * reset_to_98 - updates the value of the variable
    * the pointer points to to 98
-   * @n: pointer to the variables to update
+   * @n: pointer to the variable to update
    */
-  void reset_to_98(int *n)
-  {
-          *n = 98;
-  } 	  
+   void reset_to_98(int *n)
+   {
+	   *n = 98;
+   }  	   
