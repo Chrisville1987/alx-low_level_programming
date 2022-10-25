8a25e323b2f923bd0306dac510b7ae259e963091
chrisville1987 <christopheranumadu@gmail.com>
2022-10-24 17:45:24 -0700

    on it

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,3 @@
-julien@ubuntu:~/0x05$ cat 0-main.c
-
 #include "main.h"
 
 #include <stdio.h>
@@ -35,13 +33,3 @@ int main(void)
 			        return (0);
 
 }
-
-julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98
-
-julien@ubuntu:~/0x05$ ./0-98 
-
-n=402
-
-n=98
-
-julien@ubuntu:~/0x05$ 
