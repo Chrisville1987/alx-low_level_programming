707109283aaef33226997070c8918942dfce54a4
Chrisville1987 <christopheranumadu@gmail.com>
2022-12-14 09:38:35 -0800

    Yio dsc

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,11 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9,
- * except 2 and 4, followed by a new line
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-void print_most_numbers(void)
+int _isalpha(int c)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0');
-		}
-	}
-
-	_putchar('\n');
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
