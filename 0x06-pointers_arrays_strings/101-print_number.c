b3ea22c1fd4312a3e0d246b89170c4ba5939a3a2
Chrisville1987 <christopheranumadu@gmail.com>
2022-12-22 03:26:03 -0800

    Jam osj

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		_putchar('-')
+		_putchar('-');
 	} else
 	{
 		n1 = n;
@@ -21,4 +21,5 @@ void print_number(int n)
 		print_number(n1 / 10);
 
 	_putchar((n1 % 10) + '0');
+
 }
